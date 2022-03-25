2c51c76b0d99316500311983114e9bfd9f75ef9a
jasonchuacodes <85822633+jasonchuacodes@users.noreply.github.com>
2022-03-25 21:56:08 +0800

    Navbar hover

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -11,18 +11,20 @@
 			<div class="navbar-container collapse navbar-collapse" id="navCollapsableItems">
 				<ul class="navbar-nav">
 					<li class="nav-item">
-						<a href="#" class="nav-link" data-bs-toggle="tooltip">ABOUT
-						</a>
-						<span class="tooltip">ABOUT</span>
+						<a href="#" class="nav-link">ABOUT</a>
+						<span class="nav-item-tooltip">ABOUT</span>
 					</li>
 					<li class="nav-item">
 						<a href="#" class="nav-link">SKILLS</a>
+						<span class="nav-item-tooltip">SKILLS</span>
 					</li>
 					<li class="nav-item">
 						<a href="#" class="nav-link">LEARNING</a>
+						<span class="nav-item-tooltip">LEARNING</span>
 					</li>
 					<li class="nav-item">
 						<a href="#" class="nav-link">CONTACT</a>
+						<span class="nav-item-tooltip">CONTACT</span>
 					</li>
 				</ul>
 			</div>
@@ -36,10 +38,26 @@
 .navbar-container {
 	justify-content: end;	
 }
-.nav-item {
+
+
+.navbar-nav .nav-item{
+	position: relative;
+	/* overflow: hidden; */
 }
-.nav-item:hover {
-	background-color: red;
+.nav-item:hover .nav-item-tooltip {
+	top: -50%;
+	transform: translate(0%, 50%);
+  padding: 0.5rem;
+  text-decoration: none;
 	transition: ease-in 0.2s;
 }
+.nav-item-tooltip {
+	background-color: #ddd;
+	color: black;
+	position: absolute;
+  padding: 0.5rem;
+	top: -200%;
+	transform: translate(0%, 50%);
+}
+
 </style>
